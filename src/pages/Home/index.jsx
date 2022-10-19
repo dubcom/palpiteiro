7fb6c82fc074d1796637e6ae888e586d405f3931
Fernando Chucre <marcos@dubcom.com.br>
2022-10-18 22:29:22 -0300

    pix fix

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -16,7 +16,7 @@ export function Home() {
 
   return (
     <>
-      <header className="container content-center bg-red-700 flex justify-between max-w-5xl p-4">
+      <header className="container  bg-red-700 flex justify-between max-w-5xl p-4">
         <div>
           <a href="/" className="text-white font-bold text-2xl">
             <img src="./imgs/logoPalpite.svg" className="w-32 md:w-40" />
@@ -26,7 +26,7 @@ export function Home() {
           <button onClick={handleLoginFromGoogle} className="text-center uppercase text-white bg-red-500 hover:bg-red-300  text-mx px-4 py-2 rounded-md"> Entrar  </button>
         </div>
       </header>
-      <main className="container max-w-5xl flex flex-col justify-center items-center">
+      <main className="container max-w-5xl flex flex-col  items-center">
         <div className="flex flex-col text-white justify-center items-center">
           <img src="./imgs/logoPalpite.svg" className="w-100 md:w-50" />
           <h1 className="text-4xl text-white font-bold">Valor do premio <strong>R$ 12.985,00</strong></h1>
@@ -36,7 +36,7 @@ export function Home() {
 
         <div className=" text-white p-4 flex flex-col items-center space-y-6">
 
-          <h3 className="text-2xl justify-center md:text-center marker:font-bold">
+          <h3 className="text-2xl text-center md:text-center font-bold">
             Se você é bom em dar palpites, que tal tentar ganhar uma grana com eles?
           </h3>
 
@@ -49,16 +49,16 @@ export function Home() {
 
           </div>
         </div>
-        <div className="container max-w-5xl  center flex-1 p-4  flex md:flex-row items-center center space-y-6 md:space-y-0 md:space-x-6">
+        <div className="container max-w-5xl   p-4  flex md:space-y-0 md:space-x-6">
          
-          <div className="md:flex-row max-w-5xl px-8 sm:flex flex-col p-2 items-center bg-yellow-500">
+          <div className="flex flex-col  items-center justify-center align-center md:flex-row w-screen px-8 p-2  bg-yellow-500">
             <div>
               <img src="./imgs/logoL.png" className="w-650" />
             </div>
-            <div className=" max-w-5xl text-center px-8">
+            <div className=" max-w-5xl text-center inline-block align-middle px-8">
               <h3 className="text-3xl font-bold text-red-700">X</h3>
             </div>
-            <div>
+            <div className="align-middle">
               <img src="./imgs/logoB.png" className="w-650" />
             </div>
           </div>
