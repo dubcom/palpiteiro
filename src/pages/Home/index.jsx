06242a71c1c61972de0281d08b67d570890aba8a
Marcos Antonio <58779341+dubcom@users.noreply.github.com>
2022-10-18 23:26:35 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -53,13 +53,13 @@ export function Home() {
          
           <div className="flex flex-col  items-center justify-center align-center md:flex-row w-screen px-8 p-2  bg-yellow-500">
             <div>
-              <img src="./imgs/logoL.png" className="w-650" />
+              <img src="./imgs/logoL.png" alt="Logo Lula presidente" className="w-650" />
             </div>
             <div className=" max-w-5xl text-center inline-block align-middle px-8">
               <h3 className="text-3xl font-bold text-red-700">X</h3>
             </div>
             <div className="align-middle">
-              <img src="./imgs/logoB.png" className="w-650" />
+              <img src="./imgs/logoB.png" alt="Logo marca Bolsonaro"className="w-650" />
             </div>
           </div>
         </div>
